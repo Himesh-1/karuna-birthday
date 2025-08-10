1a210e0275abca52a6cb10923366951ef36a188f
Himesh Sharma <himeshsharma107@gmail.com>
2025-08-10 17:15:30 +0000

    make it 3000

--- src/components/confetti.tsx
+++ src/components/confetti.tsx
@@ -11,7 +11,7 @@ export function Confetti() {
   const [pieces, setPieces] = useState<ConfettiPiece[]>([]);
 
   useEffect(() => {
-    const newConfetti = Array.from({ length: 300 }).map(() => ({
+    const newConfetti = Array.from({ length: 3000 }).map(() => ({
       style: {
         '--confetti-end-x': `${(Math.random() - 0.5) * 90}vw`,
         '--confetti-end-y': `${Math.random() * 50 + 50}vh`,
