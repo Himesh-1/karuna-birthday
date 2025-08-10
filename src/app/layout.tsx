4fd48caa50282175f6a9ab159589af30fead54f5
Himesh Sharma <himeshsharma107@gmail.com>
2025-08-10 18:46:07 +0000

    tried to add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Belleza&display=swap" rel="stylesheet" />
-      </head>
       <body className="font-body antialiased">
         <DynamicBalloons />
         {children}
